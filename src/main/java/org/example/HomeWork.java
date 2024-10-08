42e8b6cd4168ce3b13c027aa3643d5cba576036c
vitaly <vitaly.e.belkin@gmail.com>
2024-10-08 21:11:31 +0300

    Vitaliy Belkin Home Work 3

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class HomeWork {
@@ -9,13 +12,13 @@ public class HomeWork {
      * <h1>Задание 1.</h1>
      * Метод возвращает подстроку максимальной длины в которой не повторяются символы
      * Если найдено несколько подстрок одинаковой длины - вернуть первую.
-     *
+     * <p>
      * Пример 1 вход= abcddcba, выход = abcd
      * Так как найдены две подстроки с неповторяющимися значениями:
      * 1) abcd
      * 2) dcba
      * Они динаковой длины, по этому возвращаем 1 строку
-     *
+     * <p>
      * Пример 2 вход= abcddcbaX, выход = dcbaX
      * Так как эта подстрока самая большая
      *
@@ -25,7 +28,29 @@ public class HomeWork {
      */
     public String findMaxSubstring(String str) {
         //TODO реализовать метод
-        return null;
+        return getSubString(str).stream().max((s1, s2) -> s1.length() - s2.length()).get();
+    }
+
+    private static List<String> getSubString(String str) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (charCount.containsKey(str.charAt(i))) {
+                fillString(str, charCount, list, i);
+            } else {
+                charCount.put(str.charAt(i), 1);
+            }
+        }
+        fillString(str, charCount, list, str.length() - 1);
+        return list;
+    }
+
+    private static void fillString(String str, HashMap<Character, Integer> charCount, List<String> list, int i) {
+        List<String> keyList = charCount.keySet().stream().map(Object::toString).collect(Collectors.toList());
+        String temp = String.join("", keyList);
+        list.add(temp);
+        charCount.clear();
+        charCount.put(str.charAt(i), 1);
     }
 
 
@@ -35,11 +60,12 @@ public class HomeWork {
      * Можно решать так же для английского алфавита, проверять что присутствуют буквы от A до Z хотя бы по одному разу.
      * <br/>
      * A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
-     *
+     * <p>
      * Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+     *
      * @see <a href="https://www.codewars.com/kata/545cedaa9943f7fe7b000048">https://www.codewars.com/kata/545cedaa9943f7fe7b000048</a>
      */
-    public boolean check(String sentence){
+    public boolean check(String sentence) {
         return false;
     }
 
